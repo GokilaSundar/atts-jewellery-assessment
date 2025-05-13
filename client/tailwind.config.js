39ad646128462dc8c0ed67e703efef3dc3064b79
GokilaSundar <gokithilaga267@gmail.com>
2025-05-13 22:21:46 +0530

    Configure and setup the styles

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        buttonColor: "#88C2BB",
+      },
+    },
   },
   plugins: [],
 };
