1d69769ee408aa7e5fc3cc0617b94cd615ff08a9
GokilaSundar <gokithilaga267@gmail.com>
2025-05-14 13:19:58 +0530

    Implement login, register routes & auth middleware

--- server/app.js
+++ server/app.js
@@ -1,12 +1,34 @@
 import "./database.js";
 import express from "express";
-import bcrypt from "bcrypt";
+import cookie from "cookie-parser";
 import jwt from "jsonwebtoken";
-import { adminModel } from "./model/adminModel.js";
+
+import adminRouter from "./routes/admin.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookie());
+
+app.use("/api", adminRouter);
+
+app.use("/api", (req, res, next) => {
+  const authCookie = req.cookies["authCookie"];
+
+  if (!authCookie) {
+    return res.status(401).send({ message: "There is no cookie" });
+  }
+
+  jwt.verify(authCookie, process.env.JWT_SECRET, (error, { adminId }) => {
+    if (error) {
+      return res.status(403).send({ message: "Invalid token" });
+    }
+
+    req.adminId = adminId;
+
+    next();
+  });
+});
 
 app.get("/api/hello", (req, res) => {
   res.send({ message: "Hello World!" });
---
+++ server/routes/admin.js
@@ -0,0 +1,86 @@
+import express from "express";
+import { adminModel } from "../model/adminModel.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  try {
+    const { adminName, adminEmail, adminPassword } = req.body;
+
+    if (!adminName) {
+      return res.status(400).send({ message: "Name is required!" });
+    }
+    if (!adminEmail) {
+      return res.status(400).send({ message: "Email is required!" });
+    }
+    if (!adminPassword) {
+      return res.status(400).send({ message: "Password is required!" });
+    }
+
+    const oldUser = await adminModel.findOne({ adminEmail });
+
+    if (oldUser) {
+      return res.status(400).send({ message: "Already have an account!" });
+    }
+
+    await adminModel.create({
+      adminName,
+      adminEmail,
+      adminPassword: bcrypt.hashSync(adminPassword, 15),
+    });
+
+    res.status(200).send({ message: "Successfully registered" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: "Failed to register", error });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { adminEmail, adminPassword: adminPassword_ } = req.body;
+
+    if (!adminEmail) {
+      return res.status(400).send({ message: "Email is required!" });
+    }
+    if (!adminPassword_) {
+      return res.status(400).send({ message: "Password is required!" });
+    }
+
+    const oldUser = await adminModel.findOne({ adminEmail });
+
+    if (!oldUser) {
+      return res.status(400).send({ message: "Admin not found!" });
+    }
+    const checkPassword = bcrypt.compareSync(
+      adminPassword_,
+      oldUser.adminPassword
+    );
+
+    if (!checkPassword) {
+      return res.status(400).send({ message: "Invalid Password!" });
+    }
+
+    const token = jwt.sign(
+      {
+        adminId: oldUser._id.toString(),
+      },
+      process.env.JWT_SECRET
+    );
+
+    res.cookie("authCookie", token, { maxAge: 900000, httpOnly: true });
+
+    const user = oldUser.toJSON();
+
+    delete user.adminPassword;
+
+    res.status(200).send(user);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: "Failed to login", error });
+  }
+});
+
+export default router;
